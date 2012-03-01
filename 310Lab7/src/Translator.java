99afcdfe218f0b73612e6d9887910cb157ac97c1
Patrick McGee <mcgeep@wit.edu>
2012-03-01 11:34:35 -0500

    FinalVersion

--- 310Lab7/src/Translator.java
+++ 310Lab7/src/Translator.java
@@ -25,9 +25,9 @@ public class Translator extends JFrame implements ActionListener
 	private JTextField input;
 	private JTextField output;
 	private Properties table;
-	public static final int WIDTH = 400;
-	public static final int HEIGHT = 300;
-	public static final int NUMBER_OF_CHAR = 20;
+	public static final int WIDTH = 600;
+	public static final int HEIGHT = 125;
+	public static final int NUMBER_OF_CHAR = 50;
 
 	// set size
 	// window destroyer
@@ -37,24 +37,31 @@ public class Translator extends JFrame implements ActionListener
 	{
 		setSize(WIDTH, HEIGHT);
 
-		loadDictionary();
 		Container contentPane = getContentPane();
 		contentPane.setBackground(Color.GRAY);
 		contentPane.setLayout(new FlowLayout());
-		
+
 		translate = new JButton("Translate");
 		translate.addActionListener(this);
 		contentPane.add(translate);
-		
-		input=new JTextField("Enter input text here");
-		contentPane.add(input);
-		output=new JTextField("Enter output text here");
-		contentPane.add(output);
 
+		input = new JTextField(NUMBER_OF_CHAR);
+		contentPane.add(input);
+		input.setText("Enter a phrase to translate here");
+		output = new JTextField(NUMBER_OF_CHAR);
+		
+		contentPane.add(output);
+		output.setText("Output Text Area");
+		loadDictionary();
 	}
 
 	// performed if action.command equals translate, translate
 
+	// create a new table of properties
+	// open the file with file stream
+	// /table load
+	// close the input
+	// for exception "output error"
 	private void loadDictionary()
 	{
 		table = new Properties();
@@ -74,28 +81,22 @@ public class Translator extends JFrame implements ActionListener
 
 	}
 
-	// create a new table of properties
-	// open the file with file stream
-	// /table load
-	// close the input
-
-	// for exception "output error"
-
 	private void translate()
 	{
 		String inputMessage = input.getText();
 		String translationWord;
 		String translationOutput = "";
 		String[] words = inputMessage.split(" ");
+
 		// create an array using split
 
 		for (String word : words)
 		{
 			translationWord = table.getProperty(word);
 			if (translationWord != null)
-				translationOutput += (translationWord);
+				translationOutput +=(translationWord)+ " ";
 			else
-				translationOutput += word + "";
+				translationOutput += word + " ";
 		}
 
 		output.setText(translationOutput);

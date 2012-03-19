b6801ebf10c84131bb0a5e222145ca23bbf36cfc
Patrick McGee <mcgeep@wit.edu>
2012-03-18 23:12:49 -0400

    updated to have main function ask user for input.

--- PubgetProject/src/WebScraper.java
+++ PubgetProject/src/WebScraper.java
@@ -1,6 +1,7 @@
-
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Scanner;
+
 import org.jsoup.Jsoup;
 import org.jsoup.helper.Validate;
 import org.jsoup.nodes.Document;
@@ -9,6 +10,7 @@ import org.jsoup.select.Elements;
 
 import java.io.IOException;
 
+// major help from http://groups.google.com/group/jsoup/browse_thread/thread/029cd38dff95e6fd?pli=1
 public class WebScraper
 {
 
@@ -47,19 +49,19 @@ public class WebScraper
 	String get_pdf_path(int volume, int issue, int pages)
 	{
 		String pdfPath = null;
-		
-		//create the abstractUrl using the get_abstract_path method
+
+		// create the abstractUrl using the get_abstract_path method
 		String abstractPath = get_abstract_path(volume, issue, pages);
-		
+
 		//
 		try
 		{
-			pdfPath=get_path_from_page(abstractPath);
+			pdfPath = get_path_from_page(abstractPath);
 		}
 		catch (IOException e)
 		{
 			// TODO Auto-generated catch block
-			System.out.println("Error in retrieving the abstract url: "+ abstractPath);
+			System.out.println("Error in retrieving the abstract url: " + abstractPath);
 			e.printStackTrace();
 		}
 
@@ -68,12 +70,41 @@ public class WebScraper
 
 	public static void main(String[] args) throws IOException
 	{
-		
-		
+
+		// A simple console user input method
 		WebScraper scraper = new WebScraper();
-		
-	//	System.out.println(scraper.get_pdf_path(2,1,13));
-		System.out.println(scraper.get_pdf_path(2,2,150));
+		Scanner input = new Scanner(System.in);
+		int volume, issue, startPage;
+		String currentLine;
+		Boolean continueYesNo = true;
+		while (continueYesNo == true)
+		{
+			System.out.print("Enter desired volume: ");
+			currentLine = input.nextLine();
+			volume = Integer.parseInt(currentLine);
+
+			System.out.print("Enter desired issue: ");
+			currentLine = input.nextLine();
+			issue = Integer.parseInt(currentLine);
+
+			System.out.print("Enter desired start page: ");
+			currentLine = input.nextLine();
+			startPage = Integer.parseInt(currentLine);
+
+			System.out.println(scraper.get_pdf_path(volume, issue, startPage));
+
+			System.out.println("Would you like to look up another pdf? Enter Y for yes and N for no");
+			currentLine = input.nextLine();
+			if (currentLine == "N" || currentLine == "n")
+			{
+				continueYesNo = false;
+			}
+
+		}
+
+		// System.out.println(scraper.get_pdf_path(2,1,13));
+
+		// System.out.println(scraper.get_pdf_path(2,2,150));
 	}
 
 }
